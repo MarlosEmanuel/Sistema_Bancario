499c30e913698f8fb7bd61dc63b4caad23dc1eeb
MarlosEmanuel <marlosemanuel28@gmail.com>
2024-04-10 19:49:13 -0300

    novo sistema bancario estruturado

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -5,6 +5,41 @@ limite = 500
 LIMITE_DE_SAQUES = 3
 numero_de_saques = 0
 
+def saque(valor_de_saque=0):
+    global saldo
+    global numero_de_saques
+    global extrato
+    global LIMITE_DE_SAQUES
+    global limite
+    
+    # Verificar se o valor de saque é maior que o limite
+    if valor_de_saque > limite:
+        print("Erro! O valor de saque é maior que o limite")
+    # Verificar se o valor de saque é menor que 0 (pois não existe saque negativo)
+    elif valor_de_saque < 0:
+        print("Erro! O valor de saque é menor que 0")
+    else:
+        # Verificar se o número de saques atingiu o limite diário
+        if numero_de_saques == LIMITE_DE_SAQUES:
+            print("Erro! O seu limite de saques diário foi atingido")
+        else:
+            saldo -= valor_de_saque
+            numero_de_saques += 1
+            extrato += f"\nSaque R${valor_de_saque}"
+            print("Valor sacado com sucesso!")
+
+def depositar(valor_de_deposito=0.0):
+        global saldo
+        global extrato
+
+        # Adicionar ao meu saldo
+        saldo += valor_de_deposito
+
+        # Adicionando a Operação ao meu Extrato
+        valor_de_deposito = round(valor_de_deposito, 2)
+        extrato += f"\nDeposito R${valor_de_deposito}"
+
+
 while True:
     menu = f"""
             Sistema Bancario
@@ -19,56 +54,17 @@ while True:
 """
     # Menu
     print(menu)
-    opcao = int(input("Digite a opcao da operacao que deseja realizar: "))
+    opcao = float(input("Digite a opcao da operacao que deseja realizar: "))
 
     # Depositar
     if opcao == 1:
-        # Criar uma variavel para armazenar a quantidade de deposito que o cliente deseja solicitar
-        valor_de_deposito_str = input("Qual o valor que deseja depositar: ")
-
-        # Substituir vírgulas por pontos (caso existam)
-        valor_de_deposito_str = valor_de_deposito_str.replace(',', '.')
-
-        # Converter para float
-        valor_de_deposito = float(valor_de_deposito_str)
-
-        # Adicionar ao meu saldo
-        saldo += valor_de_deposito
-
-        # Adicionando a Operação ao meu Extrato
-        valor_de_deposito = round(valor_de_deposito, 2)
-        extrato += f"\nDeposito R${valor_de_deposito}"
+        
+        depositar(float(input("Digite o valor que deseja depositar: ")))
 
     # Operacao de Saque
     if opcao==2:
-        
-        #Criar uma variavel para armazenar a quantidade que o cliente deseja sacar
-        valor_de_saque_str = input("Qual o valor que deseja sacar: ")
 
-        # Substituir vírgulas por pontos(caso existam)
-        valor_de_saque_str = valor_de_saque_str.replace(',','.')
-
-        # Converter para float
-        valor_de_saque = float(valor_de_saque_str)
-
-        # Verificar se o valor de saque é maior que o limite
-        if valor_de_saque>limite:
-            print(limite)
-            print(valor_de_saque)
-            print("Erro!. O valor de saque e maior que o limite")
-        # Verificar se o valor quantas operações ja foram realizadas de saque
-        elif numero_de_saques==LIMITE_DE_SAQUES:
-            print("Erro!. O seu limite de saques diario foi atingigo")
-        # Verificar se o valor de saque é menor que 0 (pois não existe saque negativo)
-        elif valor_de_saque<0:
-            print("Erro!. O valor de saque é menor que 0")
-        
-        else:
-            saldo -= valor_de_saque
-            numero_de_saques = numero_de_saques + 1
-            extrato += f"\nSaque R${valor_de_saque}"
-
-            print("Valor sacado com Sucesso !")
+        saque(float(input("Digite o valor que deseja sacar: ")))
 
     # Operacao de Extrato
     if opcao==3:
